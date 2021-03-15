62090c0db07f05e20a22ada2310220b3e08cd6d1
mikitapapok <popok11014@gmail.com>
2021-03-15 04:35:04 +0300

    feat:fix code

--- src/index.js
+++ src/index.js
@@ -39,37 +39,39 @@ module.exports = function toReadable(n) {
         "ninety",
         "hundred",
     ];
-    if(n===0){
-        string='zero'
+    if (n === 0) {
+        string = 'zero'
     }
 
     if (n > 0 && n <= 19) {
         simple = simpleNumbers[n]
-        string=simple
+        string = simple
     }
 
-    if (n >= 20 && n <=99) {
+    if (n >= 20 && n <= 99) {
         deacde = decades[Math.floor(n / 10)]
-        string=deacde
+        string = deacde
         if (n % 10 > 0) {
             simple = simpleNumbers[n % 10]
-            string+=' '+simple
+            string += ' ' + simple
         }
     }
 
-    if (n >=100 && n <= 999) {
+    if (n >= 100 && n <= 999) {
         hundreds = `${simpleNumbers[Math.floor(n/100)]} hundred`;
         a = n % 100
-        string+=hundreds
+        string += hundreds
         if (a > 0 && a <= 19) {
             deacde = simpleNumbers[a]
-            string+=' '+deacde
+            string += ' ' + deacde
         } else if (a > 19) {
             deacde = decades[Math.floor(a / 10)]
-            string+=' '+deacde
+            string += ' ' + deacde
             simple = simpleNumbers[a % 10]
-            if(a%10>0){string+=' '+simple}
+            if (a % 10 > 0) {
+                string += ' ' + simple
+            }
         }
-    }    
+    }
     return string
 }
